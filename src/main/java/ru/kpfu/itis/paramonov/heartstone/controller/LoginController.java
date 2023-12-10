5e84d53c64b9e06ac91bffe30ef7919139ab18b1
Dmitry Paramonov <dmitparam.2017@gmail.com>
2023-12-10 19:17:52 +0300

    text fields design

--- src/main/java/ru/kpfu/itis/paramonov/heartstone/controller/LoginController.java
+++ src/main/java/ru/kpfu/itis/paramonov/heartstone/controller/LoginController.java
@@ -1,7 +1,6 @@
 package ru.kpfu.itis.paramonov.heartstone.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
